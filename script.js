7b72e5be904c1633a5989780f3136231912eabd9
Peter <youngeunpc@yahoo.com>
2024-09-22 16:49:16 -0300

    changes

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const outputTxt = document.getElementById('results-div');
 const userInput = document.getElementById('user-input');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
+
+const checkValidNum = input => {
+    if (input === '') {
+        alert('Please provide a phone number');
+        return;
+    }
+}
+
+checkBtn.addEventListener('click', () => {
+    checkValidNum(userInput.value);
+});
+
+clearBtn.addEventListener('click', () => {
+    outputTxt.innerText = '';
+    userInput.value = '';
+});
